a34c78ba6afc51242ef83e861c9fe70deceeb969
gguixe <gguixe23@gmail.com>
2022-03-11 05:56:51 +0100

    added fade to black

--- Assets/Scripts/SceneTransition.cs
+++ Assets/Scripts/SceneTransition.cs
@@ -8,13 +8,43 @@ public class SceneTransition : MonoBehaviour
     public string sceneToLoad;
     public Vector2 playerPosition;
     public VectorValue StoredPosition;
+    public GameObject fadeInPanel;
+    public GameObject fadeOutPanel;
+    public float fadeWait;
+
+    void Awake()
+    {
+        if (fadeInPanel != null)
+        {
+            GameObject panel = Instantiate(fadeInPanel, Vector3.zero, Quaternion.identity) as GameObject;
+            Destroy(panel, 1);
+        }
+    }
 
     public void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Player") && !other.isTrigger)
         {
             StoredPosition.initialValue = playerPosition;
-            SceneManager.LoadScene(sceneToLoad);
+            StartCoroutine(FadeCo());
+            //SceneManager.LoadScene(sceneToLoad);
+        }
+    }
+
+    public IEnumerator FadeCo()
+    {
+        if (fadeOutPanel != null)
+        {
+            GameObject panel = Instantiate(fadeOutPanel, Vector3.zero, Quaternion.identity);
+        }
+        yield return new WaitForSeconds(fadeWait);
+
+        AsyncOperation asyncOperation = SceneManager.LoadSceneAsync(sceneToLoad);
+
+        while (!asyncOperation.isDone) //Esperamos a que cargue la escena
+        {
+            yield return null;
         }
     }
 }
+
