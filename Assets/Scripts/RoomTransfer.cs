a5f065230bd4a74a6f6569b5cdc9ce25fa2f0a0a
gguixe <gguixe23@gmail.com>
2022-03-20 20:36:14 +0100

    new room transfer - custom room size available

--- Assets/Scripts/RoomTransfer.cs
+++ Assets/Scripts/RoomTransfer.cs
@@ -5,7 +5,9 @@ using UnityEngine.UI;
 
 public class RoomTransfer : MonoBehaviour
 {
-    public Vector2 cameraChange;
+    //public Vector2 cameraChange;
+    public Vector2 CameraMinChange;
+    public Vector2 CameraMaxChange;
     public Vector3 playerChange;
     private CameraMovement cam;
     public bool needText;
@@ -29,8 +31,10 @@ public class RoomTransfer : MonoBehaviour
     {
         if(other.CompareTag("Player") && !other.isTrigger) //If room are not squared we're going to need a vector2
         {
-            cam.minPosition += cameraChange;
-            cam.maxPosition += cameraChange;
+            //cam.minPosition += cameraChange;
+            //cam.maxPosition += cameraChange;
+            cam.minPosition += CameraMinChange;
+            cam.maxPosition += CameraMaxChange;
             other.transform.position += playerChange;
 
             if(needText)
